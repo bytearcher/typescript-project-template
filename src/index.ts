57a8ae13cc52bcce5a0f8d314282dfb5bc1282c1
Panu P <pspi@users.noreply.github.com>
2017-03-15 07:18:52 +0200

    detailed promise rejection information

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 
 import Greeter from "./Greeter";
 
+// to show more information when exception is thrown in async function
+process.on("unhandledRejection", (reason: any, p: any) => {
+    console.error("Unhandled Rejection at: Promise", p, "reason:", reason);
+    process.exit(1);
+});
+
 let greeter = new Greeter();
 console.log(greeter.greet("Mike"));
